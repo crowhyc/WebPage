c76743e93b17a308777de6f061572d08e1547cf4
CrowHyc <huangyisi_cd@shishike.com>
2017-11-26 21:45:36 +0800

    [Add] project base info init

---
+++ src/main/java/com/javanewb/App.java
@@ -0,0 +1,15 @@
+package com.javanewb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
